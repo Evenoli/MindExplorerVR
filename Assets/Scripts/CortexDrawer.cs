c75a7e2db9d6de12f394da45c72ab4b495aedb51
Oliver <Oliver>
2017-02-20 17:31:51 +0000

    Added threading to querying

--- Assets/Scripts/CortexDrawer.cs
+++ Assets/Scripts/CortexDrawer.cs
@@ -10,8 +10,16 @@ using UnityEditor;
 public class CortexDrawer : MonoBehaviour {
 
     public GameObject m_MeshPartPrefab;
+
     private List<GameObject> m_MeshModelParts;
     private const int MAX_VERTECIS_PER_MESH = 64998; // Multiple of 3 to keep triangles in order
+    // Thread used for performing queries
+    private Thread m_queryThread;
+    // True if query has returned, and mesh hasn't been rendered yet.
+    private bool m_renderDue;
+    //Results of last query
+    private FLATData.FlatRes m_latestCortexData; 
+
 
     // Use this for initialization
     void Start () {
@@ -27,6 +35,8 @@ public class CortexDrawer : MonoBehaviour {
             return;
         } 
 
+
+
         /*
         print("Performing test query... \n");
 
@@ -41,10 +51,15 @@ public class CortexDrawer : MonoBehaviour {
 
     public void DrawNewQuery(float p0, float p1, float p2, float p3, float p4, float p5)
     {
-        FLATData.FlatRes cortexData = FLATData.Query(p0, p1, p2, p3, p4, p5);
-        print(cortexData.numcoords);
+        print("performing Query...");
+        m_queryThread = new Thread(() => QueryThread(p0, p1, p2, p3, p4, p5));
+        m_queryThread.Start();
+    }
 
-        DrawModel(cortexData);
+    private void QueryThread(float p0, float p1, float p2, float p3, float p4, float p5)
+    {
+        m_latestCortexData = FLATData.Query(p0, p1, p2, p3, p4, p5);
+        m_renderDue = true;
     }
 
     private void DrawModel(FLATData.FlatRes cortexData)
@@ -52,6 +67,7 @@ public class CortexDrawer : MonoBehaviour {
         // Destroy old model
         if(m_MeshModelParts.Count > 0)
         {
+            print("Destroying old mesh..");
             foreach (GameObject mesh in m_MeshModelParts)
             {
                 Destroy(mesh);
@@ -59,7 +75,7 @@ public class CortexDrawer : MonoBehaviour {
             m_MeshModelParts.Clear();
         }
             
-        print("Building Mesh...");
+        print("Query done. Building Mesh...");
 
         // Put data into vector3 form
         List<Vector3> cortexVertices = new List<Vector3>();
@@ -71,50 +87,60 @@ public class CortexDrawer : MonoBehaviour {
 
         int vertexCount = cortexVertices.Count;
 
-        int numMeshesRequired = ((vertexCount / 3) / MAX_VERTECIS_PER_MESH) + 1;
-
-        for (int i = 0; i < numMeshesRequired; i++)
+        if (vertexCount > 0)
         {
 
-            Mesh mesh = new Mesh();
-            List<Vector2> UVs = new List<Vector2>();
-            List<int> triangles = new List<int>();
-            int startInd = i * MAX_VERTECIS_PER_MESH;
-            int endInd = Math.Min(startInd + MAX_VERTECIS_PER_MESH, vertexCount - 1);
-            Vector3[] vertices = cortexVertices.GetRange(startInd, endInd - startInd).ToArray();
-            mesh.vertices = vertices;
+            int numMeshesRequired = ((vertexCount / 3) / MAX_VERTECIS_PER_MESH) + 1;
 
-            for (int j = 0; j < endInd - startInd; j++)
+            for (int i = 0; i < numMeshesRequired; i++)
             {
-                UVs.Add(new Vector2(vertices[j].x, vertices[j].z));
 
-                if (j < endInd - startInd - 2 && (j % 3 == 0))
+                Mesh mesh = new Mesh();
+                List<Vector2> UVs = new List<Vector2>();
+                List<int> triangles = new List<int>();
+                int startInd = i * MAX_VERTECIS_PER_MESH;
+                int endInd = Math.Min(startInd + MAX_VERTECIS_PER_MESH, vertexCount - 1);
+                Vector3[] vertices = cortexVertices.GetRange(startInd, endInd - startInd).ToArray();
+                mesh.vertices = vertices;
+
+                for (int j = 0; j < endInd - startInd; j++)
                 {
-                    triangles.Add(j);
-                    triangles.Add(j + 1);
-                    triangles.Add(j + 2);
+                    UVs.Add(new Vector2(vertices[j].x, vertices[j].z));
+
+                    if (j < endInd - startInd - 2 && (j % 3 == 0))
+                    {
+                        triangles.Add(j);
+                        triangles.Add(j + 1);
+                        triangles.Add(j + 2);
+                    }
                 }
+
+                mesh.triangles = triangles.ToArray();
+                mesh.uv = UVs.ToArray();
+                mesh.RecalculateNormals();
+
+                GameObject meshPart = Instantiate(m_MeshPartPrefab);
+                meshPart.transform.SetParent(transform);
+
+                MeshFilter mf = meshPart.GetComponent<MeshFilter>();
+                if (mf)
+                {
+                    mf.mesh = mesh;
+                }
+
+                m_MeshModelParts.Add(meshPart);
             }
-
-            mesh.triangles = triangles.ToArray();
-            mesh.uv = UVs.ToArray();
-            mesh.RecalculateNormals();
-
-            GameObject meshPart = Instantiate(m_MeshPartPrefab);
-            meshPart.transform.SetParent(transform);
-
-            MeshFilter mf = meshPart.GetComponent<MeshFilter>();
-            if (mf)
-            {
-                mf.mesh = mesh;
-            }
-
-            m_MeshModelParts.Add(meshPart);
         }
+
+        print("Done");
     }
 
     // Update is called once per frame
     void Update () {
-		
-	}
+        if (m_renderDue)
+        {
+            DrawModel(m_latestCortexData);
+            m_renderDue = false;
+        }
+    }
 }
